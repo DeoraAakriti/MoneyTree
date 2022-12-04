fd3c4ed15171f423c8d897f5dc537d3775104444
Pragna M <millp01@pfw.edu>
2022-12-04 18:30:43 -0500

    Changes to UserController

--- API/moneytreeapi/controllers/UserController.js
+++ API/moneytreeapi/controllers/UserController.js
@@ -1,48 +1,52 @@
 import UserService from "../services/UserService.js";
 
 const UserController = {
+  getUserById: async (req, res, next) => {
+    try {
+      //   console.log(req);
+      var response = await UserService.getUserById(req.params.userId);
+      console.log(response);
+      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ status: 400, mesage: e.message });
+    }
+  },
+  createNewUser: async (req, res, next) => {
+    try {
+      var response = await UserService.createNewUser(req.body);
+      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ status: 400, mesage: e.message });
+    }
+  },
+  deleteUser: async (req, res, next) => {
+    try {
+      var response = await UserService.deleteUser(req.params.userId);
+      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ status: 400, mesage: e.message });
+    }
+  },
+  updateUser: async (req, res, next) => {
+    try {
+      var response = await UserService.updateUser(req.params.userId, req.body);
+      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ status: 400, message: e.message });
+    }
+  },
 
-    getUserById: async (req, res, next) => {
-        try{
-            var response = await UserService.getUserById(req.params.userId);
-            return res.status(200).json(response);
-        } catch(e) {
-            return res.status(400).json({status: 400, mesage: e.message});
-        }
-    },
-    createNewUser: async (req, res, next) => {
-        try{
-            var response = await UserService.createNewUser(req.body);
-            return res.status(200).json(response);
-        } catch(e) {
-            return res.status(400).json({status: 400, mesage: e.message});
-        }
-    },
-    deleteUser: async (req, res, next) => {
-        try{
-            var response = await UserService.deleteUser(req.params.userId);
-            return res.status(200).json(response);
-        } catch(e) {
-            return res.status(400).json({status: 400, mesage: e.message});
-        }
-    },
-    updateUser: async (req, res, next) => {
-        try {
-            var response = await UserService.updateUser(req.params.userId, req.body);
-            return res.status(200).json(response);
-        } catch(e) {
-            return res.status(400).json({status: 400, message: e.message});
-        }
-    },
-
-    getUserByuserName: async (req, res, next) => {
-        try{
-            var response = await UserService.getUserByuserName(req.body.userName, req.body.password);
-            return res.status(200).json(response);
-        } catch(e) {
-            return res.status(400).json({status: 400, mesage: e.message});
-        }
-    },
-}
+  getUserByuserName: async (req, res, next) => {
+    try {
+      var response = await UserService.getUserByuserName(
+        req.body.userName,
+        req.body.password
+      );
+      return res.status(200).json(response);
+    } catch (e) {
+      return res.status(400).json({ status: 400, mesage: e.message });
+    }
+  },
+};
 
 export default UserController;
